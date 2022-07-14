41942abd40282623378fbb93901a952c5b1d3f2c
Afonso Boto <afonsomsboto@gmail.com>
2022-07-14 19:26:15 +0100

    alinea c

--- Práticas/Lab-2/E4/ex4/src/main/java/org/example/App.java
+++ Práticas/Lab-2/E4/ex4/src/main/java/org/example/App.java
@@ -1,36 +1,38 @@
 package org.example;
 
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Indexes;
+import com.mongodb.client.model.*;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 
 
-public class App
-{
+public class App {
+    private static BsonField sum;
     MongoClient mongoClient;
     MongoDatabase database;
     MongoCollection<Document> collection;
-    public App(){
+
+    public App() {
         mongoClient = MongoClients.create();
 
         database = mongoClient.getDatabase("cbd");
-        collection = database.getCollection("cbd");
+        collection = database.getCollection("restaurants");
 
     }
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
 
         App app = new App();
         testSpeed(app);
@@ -38,7 +40,72 @@ public class App
         app.createIndexAscending("localidade");
         app.createIndexText("nome");
         testSpeed(app);
-        
+
+        System.out.println(app.collection.count());
+
+        //17) de exercicio 2
+        System.out.println("EXERCICIO 4 _ C");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("1 - PRIMEIRO");
+        List<Document> result = new ArrayList<>();
+        app.collection.aggregate(
+                List.of(Aggregates.project(new Document(Map.of("nome", 1, "localidade", 1, "gastronomia", 1))),
+                Aggregates.sort(new Document(
+                        Map.of("localidade", 1, "nome", 1)
+                )))).into(result);
+
+        result.forEach(x -> System.out.println("Localidade: " + x.get("localidade") + ", Nome: " + x.get("nome") + ", Gastronomia: " + x.get("gastronomia")));
+        System.out.println("-----------------------------------------------------");
+        //18) de exercicio 2
+        System.out.println("2 - SEGUNDO");
+        result.clear();
+        app.collection.aggregate(
+                List.of(Aggregates.project(Projections.include("nome", "localidade", "gastronomia", "grades.grade")),
+                        Aggregates.match(Filters.eq("localidade", "Brooklyn")),
+                        Aggregates.match(Filters.ne("gastronomia", "American")),
+                        Aggregates.match(Filters.eq("grades.grade", "A")),
+                        Aggregates.sort(Sorts.descending("gastronomia"))
+        )).into(result);
+        result.forEach(x -> System.out.println("Localidade: " + x.get("localidade") + ", Nome: " + x.get("nome") + ", Gastronomia: " + x.get("gastronomia") + ", Grade: " + x.get("grades")));
+        System.out.println("-----------------------------------------------------");
+
+        //19) de exercicio 2
+        System.out.println("3 - TERCEIRO");
+        result.clear();
+        app.collection.aggregate(List.of(
+                Aggregates.group("$localidade", Accumulators.sum("total", 1)),
+                Aggregates.sort(Sorts.descending("total"))
+
+        )).into(result);
+        result.forEach(x -> System.out.println("Localidade: " + x.get("_id") + ", Total: " + x.get("total")));
+        System.out.println("-----------------------------------------------------");
+
+        //20) de exercicio 2
+        System.out.println("4 - QUARTO");
+        result.clear();
+        app.collection.aggregate(List.of(
+                Aggregates.unwind("$grades"),
+                Aggregates.group("$nome", Accumulators.avg("media", "$grades.score")),
+                Aggregates.match(Filters.gt("media", 30))
+        )).into(result);
+
+        result.forEach(x -> System.out.println("Nome: " + x.get("_id") + ", Media: " + x.get("media")));
+
+        System.out.println("-----------------------------------------------------");
+
+        //21) de exercicio 2
+        System.out.println("5 - QUINTO");
+        result.clear();
+
+        result = app.search(Filters.and(
+               Filters.eq("gastronomia", "Portuguese"),
+               Filters.expr(new BasicDBObject("$gt", List.of(
+                       new BasicDBObject("$sum", "$grades.score"),
+                       50
+               ))),
+               Filters.lt("address.coord.0", -60)));
+
+        result.forEach(x -> System.out.println("Nome: " + x.get("nome") + ", Gastronomia: " + x.get("gastronomia") + ", localidade: " + x.get("localidade")));
     }
 
     private void createIndexAscending(String field) {
@@ -85,7 +152,7 @@ public class App
         return res;
     }
 
-    public static void testSpeed(App app){
+    public static void testSpeed(App app) {
         Instant start = Instant.now();
         app.search(Filters.eq("localidade", "Manhattan"));
         app.search(Filters.eq("gastronomia", "American"));
