4cbdb0ddccb2f7930982ab58432839fb51c31370
Afonso Boto <afonsomsboto@gmail.com>
2022-07-14 17:59:08 +0100

    exercicio 4 alinea b

--- Práticas/Lab-2/E4/ex4/src/main/java/org/example/App.java
+++ Práticas/Lab-2/E4/ex4/src/main/java/org/example/App.java
@@ -5,10 +5,12 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Indexes;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -29,8 +31,24 @@ public class App
     }
     public static void main( String[] args )
     {
+
         App app = new App();
+        testSpeed(app);
+        app.createIndexAscending("gastronomia");
+        app.createIndexAscending("localidade");
+        app.createIndexText("nome");
+        testSpeed(app);
         
+    }
+
+    private void createIndexAscending(String field) {
+        Bson index = Indexes.ascending(field);
+        collection.createIndex(index);
+    }
+
+    private void createIndexText(String field) {
+        Bson index = Indexes.text(field);
+        collection.createIndex(index);
     }
 
     public void insert(Document document) {
@@ -65,5 +83,15 @@ public class App
         List<Document> res = new ArrayList<>();
         collection.find(filter).forEach((Consumer<Document>) res::add);
         return res;
+    }
+
+    public static void testSpeed(App app){
+        Instant start = Instant.now();
+        app.search(Filters.eq("localidade", "Manhattan"));
+        app.search(Filters.eq("gastronomia", "American"));
+        app.search(Filters.eq("nome", "Pao"));
+        Instant end = Instant.now();
+
+        System.out.println("Time: " + (end.toEpochMilli() - start.toEpochMilli()));
     }
 }
